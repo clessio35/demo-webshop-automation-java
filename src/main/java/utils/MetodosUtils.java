648315da4f83f1a698b73d0be9ce0463c4ba6f8d
Clessio <clessio.silva1989@gmail.com>
2025-06-15 21:32:40 -0300

    config  CI

--- src/main/java/utils/MetodosUtils.java
+++ src/main/java/utils/MetodosUtils.java
@@ -133,7 +133,7 @@ public class MetodosUtils {
     }
 
     public void clickElementByXpath(String xpath) {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
         By locator = By.xpath(xpath);
         WebElement elemento = wait.until(ExpectedConditions.elementToBeClickable(locator));
         try {
@@ -146,7 +146,7 @@ public class MetodosUtils {
 
 
 	public void clickElementById(String id) {
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
 		By locator = By.id(id);
 		WebElement elemento = wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
 		wait.until(ExpectedConditions.elementToBeClickable(locator));
@@ -154,7 +154,7 @@ public class MetodosUtils {
 	}
 
 	public void typeElement(String locatorString, String text) {
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
 		By locator = getByFromString(locatorString);
 		WebElement elemento = wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
 		elemento.clear();
@@ -172,7 +172,7 @@ public class MetodosUtils {
 	}
 
 	public void validateText(String expectedText, String element) {
-		WebElement report = new WebDriverWait(driver, Duration.ofSeconds(10))
+		WebElement report = new WebDriverWait(driver, Duration.ofSeconds(20))
 				.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(element)));
 		String text = report.getText().trim();
 		Assert.assertEquals(expectedText, text);
